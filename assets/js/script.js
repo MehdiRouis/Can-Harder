c3ebab8e42d2182dc7c8aa50dbbf9feabc3b75ef
MehdiRouis <mehdi.rouis.1@laposte.net>
2018-10-26 13:38:20 +0200

    Debug false

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    physics: { default: 'arcade', arcade: { gravity: { y: 300 }, debug: true } },
+    physics: { default: 'arcade', arcade: { gravity: { y: 300 }, debug: false } },
     scene: { preload: preload, create: create, update: update }
   };
 

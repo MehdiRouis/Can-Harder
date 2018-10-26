7bdce97c58b0d811823d93c482521de496f94939
MehdiRouis <mehdi.rouis.1@laposte.net>
2018-10-26 13:20:25 +0200

    Finalisation du projet

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    physics: { default: 'arcade', arcade: { gravity: { y: 300 }, debug: false } },
+    physics: { default: 'arcade', arcade: { gravity: { y: 300 }, debug: true } },
     scene: { preload: preload, create: create, update: update }
   };
 
@@ -18,6 +18,8 @@
   var z,q,d;
   var lastButton = 'Right';
   var seconds = 0;
+  var level = 1;
+  var eggAmmos = 0;
 
   var game = new Phaser.Game(config);
 
@@ -34,9 +36,10 @@
     this.load.image('transition1', './assets/ressources/transition/transition1.png');
     this.load.image('ground', 'assets/ressources/platform.png');
     this.load.spritesheet('star', 'assets/ressources/eggs.png', { frameWidth : 35, frameHeight : 39 });
+    this.load.spritesheet('eggAmmo', 'assets/ressources/eggs.png', { frameWidth : 39, frameHeight : 35 });
     this.load.image('bomb', 'assets/ressources/bomb.png');
-    this.load.spritesheet('dude', 'assets/ressources/duck.png', { frameWidth: 33, frameHeight: 33 });
-    this.load.spritesheet('monsters', 'assets/ressources/hero_spritesheet.png', { frameWidth: 36, frameHeight: 42 });
+    this.load.spritesheet('dude', 'assets/ressources/duck.png', { frameWidth: 33, frameHeight: 34 });
+    this.load.spritesheet('monstersLevel1', 'assets/ressources/hero_spritesheet.png', { frameWidth: 36, frameHeight: 42 });
     this.load.image('invisible-wall', 'assets/ressources/invisible_wall.png');
   }
 
@@ -93,6 +96,7 @@
     platforms.create(6200, 295, 'plate3');
     // The player and its settings
     player = this.physics.add.sprite(100, 450, 'dude');
+    player.setSize(28, 23)
     //  Our player animations, turning, walking left and walking right.
     this.anims.create({
       key: 'right',
@@ -107,18 +111,18 @@
     });
     this.anims.create({
       key: 'turnLeft',
-      frames: [{ key: 'dude', frame: 6 }],
+      frames: [{ key: 'dude', frame: 8 }],
       frameRate: 20
     });
     this.anims.create({
       key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 13 }),
+      frames: this.anims.generateFrameNumbers('dude', { start: 7, end: 13 }),
       frameRate: 10,
       repeat: -1
     });
 
-    monsters = [this.physics.add.sprite(600, 450, 'monsters'),this.physics.add.sprite(10, 10, 'monsters'),this.physics.add.sprite(60, 200, 'monsters'),this.physics.add.sprite(500, 350, 'monsters')];
-    invisibleWalls = [this.physics.add.sprite(0, 100, 'invisible_wall'),this.physics.add.sprite(400, 100, 'invisible_wall'),this.physics.add.sprite(400, 380, 'invisible_wall')];
+    monstersLevel1 = [this.physics.add.sprite(600, 450, 'monstersLevel1'),this.physics.add.sprite(10, 10, 'monstersLevel1'),this.physics.add.sprite(60, 200, 'monstersLevel1'),this.physics.add.sprite(500, 350, 'monstersLevel1'),this.physics.add.sprite(700, 150, 'monstersLevel1')];
+    invisibleWalls = [this.physics.add.sprite(0, 100, 'invisible_wall'),this.physics.add.sprite(400, 100, 'invisible_wall'),this.physics.add.sprite(400, 380, 'invisible_wall'),this.physics.add.sprite(550, 190, 'invisible_wall'),this.physics.add.sprite(950, 190, 'invisible_wall')];
     for(var i = 0; i < invisibleWalls.length; i++) {
       invisibleWalls[i].body.allowGravity = false;
       invisibleWalls[i].body.immovable = true;
@@ -126,19 +130,19 @@
     }
     this.anims.create({
       key:'monsterLeft',
-      frames: this.anims.generateFrameNumbers('monsters', { start : 1, end : 5 }),
+      frames: this.anims.generateFrameNumbers('monstersLevel1', { start : 1, end : 5 }),
       frameRate: 10,
       repeat: -1
     });
     this.anims.create({
       key:'monsterRight',
-      frames: this.anims.generateFrameNumbers('monsters', { start : 1, end : 5 }),
+      frames: this.anims.generateFrameNumbers('monstersLevel1', { start : 1, end : 5 }),
       frameRate: 10,
       repeat: -1
     });
     this.anims.create({
       key: 'egg',
-      frames: [{ key: 'star', frame: 0 }],
+      frames: [{ key: 'star', frame: 13 }],
       frameRate: 10
     });
     //  Input Events
@@ -146,42 +150,49 @@
     z = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
     q = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
     d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    f = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
     //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-    stars = this.physics.add.group({ key: 'star', frame : 5, repeat: 11, setXY: { x: 50, y: 0, stepX: 70 } });
+    stars = this.physics.add.group({ key: 'star', frame : 0, repeat: 11, setXY: { x: 50, y: 0, stepX: 70 } });
     stars.children.iterate(function (child) {
       //  Give each star a slightly different bounce
       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
     });
     bombs = this.physics.add.group();
-    gunShots = this.physics.add.group();
+    eggs = this.physics.add.group();
     //  The score
-    scoreText = this.add.text(16, 16, 'Temps: ' + seconds + ' secondes', { fontSize: '32px', fill: '#000' });
+    timeText = $('#timeText');
+    timeText.text('Temps: ' + seconds + ' secondes').css({ position : 'absolute', top : '10px', left : '10px', fontSize : '25px', textAlign : 'left' });
+    scoreText = $('#scoreText');
+    scoreText.text('Score: ' + score + ' points').css({ position : 'absolute', top : '40px', left : '10px', fontSize : '25px', textAlign : 'left' });
+    levelText = $('#levelText');
+    levelText.text('Niveau: ' + level).css({ position : 'absolute', top : '70px', left : '10px', fontSize : '25px', textAlign : 'left' });
+    eggAmmosText = $('#ammos');
+    eggAmmosText.text('Munitions: ' + eggAmmos).css({ position : 'absolute', top : '100px', left : '10px', fontSize : '25px', textAlign : 'left' });
     //  Collide the player and the stars with the platforms
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(stars, platforms);
     this.physics.add.collider(bombs, platforms);
-    this.physics.add.collider(gunShots, platforms);
-    this.physics.add.collider(gunShots, bombs);
-    this.physics.add.collider(gunShots, stars);
-    this.physics.add.collider(monsters, platforms);
-    this.physics.add.collider(monsters, invisibleWalls);
-    this.physics.add.collider(monsters, monsters);
-    this.physics.add.collider(player, monsters, die, null, this)
+    this.physics.add.collider(monstersLevel1, platforms);
+    this.physics.add.collider(monstersLevel1, invisibleWalls);
+    this.physics.add.collider(monstersLevel1, monstersLevel1);
+    this.physics.add.collider(eggs, platforms)
+    this.physics.add.collider(eggs, monstersLevel1, killMonster, null, this);
+    this.physics.add.collider(player, monstersLevel1, die, null, this)
     //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
     this.physics.add.overlap(player, stars, collectStar, null, this);
     this.physics.add.collider(player, bombs, die, null, this);
-    this.cameras.main.startFollow(player)
+    this.cameras.main.startFollow(player);
     player.setCollideWorldBounds(true);
-    for(var i = 0;i < monsters.length; i++) {
-      monsters[i].setCollideWorldBounds(true);
-      monsters[i].setVelocityX(-160);
-      monsters[i].play('monsterLeft');
+    for(var i = 0;i < monstersLevel1.length; i++) {
+      monstersLevel1[i].setCollideWorldBounds(true);
+      monstersLevel1[i].setVelocityX(-160);
+      monstersLevel1[i].play('monsterLeft');
     }
   }
   setInterval(function() {
     seconds += 1;
     var secondText = (seconds === 1) ? 'seconde' : 'secondes';
-    scoreText.setText('Temps: ' + seconds + ' ' + secondText);
+    timeText.text('Temps: ' + seconds + ' ' + secondText);
   }, 1000);
   function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max + 1));
@@ -191,33 +202,14 @@
       alert('Tu as perdu!');
       return;
     }
-    if(monsters[0].body.touching.right || monsters[0].body.blocked.right) {
-      monsters[0].setVelocityX(-160);
-      monsters[0].play('monsterLeft');
-    } else if(monsters[0].body.touching.left || monsters[0].body.blocked.left) {
-      monsters[0].setVelocityX(160);
-      monsters[0].play('monsterRight');
-    }
-    if(monsters[1].body.touching.right || monsters[1].body.blocked.right) {
-      monsters[1].setVelocityX(-160);
-      monsters[1].play('monsterLeft');
-    } else if(monsters[1].body.touching.left || monsters[1].body.blocked.left) {
-      monsters[1].setVelocityX(160);
-      monsters[1].play('monsterRight');
-    }
-    if(monsters[2].body.touching.right || monsters[2].body.blocked.right) {
-      monsters[2].setVelocityX(-160);
-      monsters[2].play('monsterLeft');
-    } else if(monsters[2].body.touching.left || monsters[2].body.blocked.left) {
-      monsters[2].setVelocityX(160);
-      monsters[2].play('monsterRight');
-    }
-    if(monsters[3].body.touching.right || monsters[3].body.blocked.right) {
-      monsters[3].setVelocityX(-160);
-      monsters[3].play('monsterLeft');
-    } else if(monsters[3].body.touching.left || monsters[3].body.blocked.left) {
-      monsters[3].setVelocityX(160);
-      monsters[3].play('monsterRight');
+    for(var i = 0; i < monstersLevel1.length; i++) {
+      if(monstersLevel1[i].body.touching.right || monstersLevel1[0].body.blocked.right) {
+        monstersLevel1[i].setVelocityX(-110);
+        monstersLevel1[i].play('monsterLeft');
+      } else if(monstersLevel1[i].body.touching.left || monstersLevel1[i].body.blocked.left) {
+        monstersLevel1[i].setVelocityX(110);
+        monstersLevel1[i].play('monsterRight');
+      }
     }
     if(cursors.left.isDown || q.isDown) {
       player.setVelocityX(-160);
@@ -236,17 +228,40 @@
         player.setVelocityY(-360);
       }
     }
+    // if(f.isDown) {
+    //     if(eggAmmos > 0) {
+    //       if(eggs.countActive(true) === 0) {
+    //         var egg = eggs.create(player.x, player.y, 'eggAmmo');
+    //         eggAmmos--;
+    //         eggAmmosText.text('Munitions: ' + eggAmmos);
+    //         if(lastButton === 'Right') {
+    //           egg.setVelocityX(600).setVelocityY(-100);
+    //         } else if (lastButton === 'Left') {
+    //           egg.setVelocityX(-600).setVelocityY(-100);
+    //         }
+    //         egg.setCollideWorldBounds(true);
+    //     }
+    //   }
+    // }
+    eggs.children.iterate(function (child) {
+      if(child.body.touching.right || child.body.blocked.right || child.body.touching.left || child.body.blocked.left) {
+        child.disableBody(true, true);
+        child.setVisible(false);
+      }
+    });
   }
 
-  function collectStar(player, star)
-{
+  function collectStar(player, star) {
     star.disableBody(true, true);
+    eggAmmos++;
+    eggAmmosText.text('Munitions: ' + eggAmmos);
 
     //  Add and update the score
     score += 10;
-    scoreText.setText('Score: ' + score);
-
+    scoreText.text('Score: ' + score);
     if(stars.countActive(true) === 0) {
+        level++;
+        levelText.text('Niveau: ' + level);
         //  A new batch of stars to collect
         stars.children.iterate(function (child) {
             child.enableBody(true, child.x, 0, true, true);
@@ -255,10 +270,23 @@
         var bomb = bombs.create(x, 16, 'bomb');
         bomb.setBounce(1);
         bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.setVelocity(Phaser.Math.Between(-500, 500), 20);
         bomb.allowGravity = false;
     }
 }
+
+  function killMonster(monster, egg) {
+    monster.disableBody(true, true);
+    monster.setTint(0xff0000);
+    monster.setVisible(false);
+    for(var i = 0; i < monstersLevel1.length; i++) {
+      if(monstersLevel1[i] === monster) {
+
+      }
+    }
+    score += 100;
+    scoreText.text('Score: ' + score);
+  }
 
   function die(player, bomb)
   {
